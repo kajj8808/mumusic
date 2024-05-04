618149bc39af29be6f6bb3b7ed5b5e5bfa7b90eb
kajj8808 <a01039388808@gmail.com>
2024-05-05 00:37:57 +0900

    🐛 skip interaction에 답변을 넣지 않은 문제 해결

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -3,7 +3,9 @@ import { Guild, Interaction } from "discord.js";
 
 export async function skipHandler(interaction: Interaction) {
   if (!interaction.isButton()) return;
+  interaction.deferReply();
   const guild = interaction.guild as Guild;
   const queue = useQueue(guild.id);
   queue?.node.skip();
+  interaction.deleteReply();
 }
